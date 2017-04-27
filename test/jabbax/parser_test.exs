8d9ef371ea0e53b93fbc12d046904b1089730071
Karol Sluszniak <ksluszniak@gmail.com>
2017-04-27 16:46:36 +0200

    Refactor tests to avoid unneeded setup

--- test/jabbax/parser_test.exs
+++ test/jabbax/parser_test.exs
@@ -3,39 +3,37 @@ defmodule Jabbax.ParserTest do
   use Plug.Test
   use Jabbax.Document
 
+  @sample_json %{
+    "data" => %{
+      "id" => "1",
+      "type" => "user",
+      "attributes" => %{
+        "name" => "Sample User"
+      }
+    },
+    "jsonapi" => %{
+      "version" => "1.0"
+    }
+  }
+
   def parse(conn, parsers) do
     Plug.Parsers.call(conn, Plug.Parsers.init(parsers: parsers, json_decoder: Poison))
   end
 
-  setup do
-    {:ok, json: %{
-      "data" => %{
-        "id" => "1",
-        "type" => "user",
-        "attributes" => %{
-          "name" => "Sample User"
-        }
-      },
-      "jsonapi" => %{
-        "version" => "1.0"
-      }
-    }}
-  end
-
-  test "JSON API document", %{json: json} do
+  test "JSON API document" do
     connection =
       :post
-      |> conn("/", Poison.encode!(json))
+      |> conn("/", Poison.encode!(@sample_json))
       |> put_req_header("content-type", "application/vnd.api+json")
       |> parse([Jabbax.Parser])
 
-    assert connection.body_params == json
+    assert connection.body_params == @sample_json
   end
 
-  test "plain JSON content type without JSON parser", %{json: json} do
+  test "plain JSON content type without JSON parser" do
     connection =
       :post
-      |> conn("/", Poison.encode!(json))
+      |> conn("/", Poison.encode!(@sample_json))
       |> put_req_header("content-type", "application/json")
 
     assert_raise(Plug.Parsers.UnsupportedMediaTypeError, fn ->
@@ -43,10 +41,10 @@ defmodule Jabbax.ParserTest do
     end)
   end
 
-  test "no content type", %{json: json} do
+  test "no content type" do
     connection =
       :post
-      |> conn("/", Poison.encode!(json))
+      |> conn("/", Poison.encode!(@sample_json))
       |> parse([Jabbax.Parser])
 
     assert connection.body_params == %{}
--- test/jabbax/plug_test.exs
+++ test/jabbax/plug_test.exs
@@ -3,69 +3,60 @@ defmodule Jabbax.PlugTest do
   use Plug.Test
   use Jabbax.Document
 
+  @sample_json %{
+    "data" => %{
+      "id" => "1",
+      "type" => "user",
+      "attributes" => %{
+        "name" => "Sample User"
+      }
+    },
+    "jsonapi" => %{
+      "version" => "1.0"
+    }
+  }
+
+  @sample_doc %Document{
+    data: %Resource{
+      id: "1",
+      type: "user",
+      attributes: %{
+        "name" => "Sample User"
+      }
+    },
+    jsonapi: %{version: "1.0"}
+  }
+
   def parse(conn, parsers) do
     Plug.Parsers.call(conn, Plug.Parsers.init(parsers: parsers, json_decoder: Poison))
   end
 
-  setup do
-    {:ok, json: %{
-      "data" => %{
-        "id" => "1",
-        "type" => "user",
-        "attributes" => %{
-          "name" => "Sample User"
-        }
-      },
-      "jsonapi" => %{
-        "version" => "1.0"
-      }
-    }}
-  end
-
-  test "JSON API document", %{json: json} do
+  test "JSON API document" do
     connection =
       :post
-      |> conn("/", Poison.encode!(json))
+      |> conn("/", Poison.encode!(@sample_json))
       |> put_req_header("content-type", "application/vnd.api+json")
       |> parse([Jabbax.Parser])
       |> Jabbax.Plug.call(Jabbax.Plug.init(nil))
 
-    assert connection.assigns[:doc] == %Document{
-      data: %Resource{
-        id: "1",
-        type: "user",
-        attributes: %{
-          "name" => "Sample User"
-        }
-      },
-      jsonapi: %{version: "1.0"}
-    }
+    assert connection.assigns[:doc] == @sample_doc
   end
 
-  test "JSON API document with custom assign name", %{json: json} do
+  test "JSON API document with custom assign name" do
     connection =
       :post
-      |> conn("/", Poison.encode!(json))
+      |> conn("/", Poison.encode!(@sample_json))
       |> put_req_header("content-type", "application/vnd.api+json")
       |> parse([Jabbax.Parser])
       |> Jabbax.Plug.call(Jabbax.Plug.init(assign: :custom_doc))
 
-    assert connection.assigns[:custom_doc] == %Document{
-      data: %Resource{
-        id: "1",
-        type: "user",
-        attributes: %{
-          "name" => "Sample User"
-        }
-      },
-      jsonapi: %{version: "1.0"}
-    }
+    assert connection.assigns[:custom_doc] == @sample_doc
   end
 
-  test "other content type", %{json: json} do
+  test "other content type" do
     connection =
       :post
-      |> conn("/", Poison.encode!(json))
+      |> conn("/", Poison.encode!(@sample_json))
       |> put_req_header("content-type", "application/json")
       |> parse([:json, Jabbax.Parser])
       |> Jabbax.Plug.call(Jabbax.Plug.init(nil))
