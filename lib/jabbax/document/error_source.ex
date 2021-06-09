71cfbca0dc469561fc42ea45fc0a1eb3a93319e1
Paweł Świątkowski <katafrakt@vivaldi.net>
2021-06-10 00:12:14 +0200

    Add support for prefixing attribute in ErrorSource.from_attribute
    This is required to have support for nested errors in changesets in Surgex.Changeset.

--- lib/jabbax/document/error_source.ex
+++ lib/jabbax/document/error_source.ex
@@ -1,7 +1,13 @@
 defmodule Jabbax.Document.ErrorSource do
   defstruct [:pointer, :parameter]
 
-  def from_attribute(attribute) do
-    %__MODULE__{pointer: "/data/attributes/#{attribute}"}
+  def from_attribute(attribute, prefix \\ []) do
+    attribute_prefix =
+      prefix
+      |> Enum.map(&"#{&1}/")
+      |> Enum.join("")
+
+    pointer = "/data/attributes/#{attribute_prefix}#{attribute}"
+    %__MODULE__{pointer: pointer}
   end
 end
--- test/jabbax/document/error_source_test.exs
+++ test/jabbax/document/error_source_test.exs
@@ -2,9 +2,20 @@ defmodule Jabbax.Document.ErrorSourceTest do
   use ExUnit.Case
   alias Jabbax.Document.ErrorSource
 
-  test "#from_attribute" do
-    assert ErrorSource.from_attribute(:first_name) ==
-      %ErrorSource{pointer: "/data/attributes/first_name"}
+  describe "from_attribute/2" do
+    test "simple pointer" do
+      assert ErrorSource.from_attribute(:first_name) ==
+               %ErrorSource{pointer: "/data/attributes/first_name"}
+    end
+
+    test "pointer with one-level prefix" do
+      assert ErrorSource.from_attribute(:city, ["address"]) ==
+               %ErrorSource{pointer: "/data/attributes/address/city"}
+    end
+
+    test "pointer with multi-level prefix" do
+      assert ErrorSource.from_attribute(:type, ["decision", "rationale", "reasoning"]) ==
+               %ErrorSource{pointer: "/data/attributes/decision/rationale/reasoning/type"}
+    end
   end
 end
-
