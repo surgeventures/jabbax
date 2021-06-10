2dffe8efed5f9bbf94561b1335ecdca3654f0065
Paweł Świątkowski <katafrakt@vivaldi.net>
2021-06-10 10:03:42 +0200

    Apply mix format

--- lib/jabbax.ex
+++ lib/jabbax.ex
@@ -6,18 +6,18 @@ defmodule Jabbax do
   def decode!(input) do
     input
     |> Application.get_env(:jabbax, :json_decoder).decode!
-    |> Deserializer.call
+    |> Deserializer.call()
   end
 
   def encode!(input) do
     input
-    |> Serializer.call
+    |> Serializer.call()
     |> Application.get_env(:jabbax, :json_encoder).encode!
   end
 
   def encode_to_iodata!(input) do
     input
-    |> Serializer.call
+    |> Serializer.call()
     |> Application.get_env(:jabbax, :json_encoder).encode_to_iodata!
   end
 end
--- test/jabbax/document/error_source_test.exs
+++ test/jabbax/document/error_source_test.exs
@@ -4,6 +4,6 @@ defmodule Jabbax.Document.ErrorSourceTest do
 
   test "#from_attribute" do
     assert ErrorSource.from_attribute(:first_name) ==
-      %ErrorSource{pointer: "/data/attributes/first_name"}
+             %ErrorSource{pointer: "/data/attributes/first_name"}
   end
 end
