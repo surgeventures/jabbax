1b2b55ab646e58cbee8ee7c0fc4ea499ec98160e
Paweł Świątkowski <katafrakt@vivaldi.net>
2021-06-12 15:21:48 +0200

    Add ex_doc so we can deploy dev version

--- mix.exs
+++ mix.exs
@@ -33,9 +33,10 @@ defmodule Jabbax.Mixfile do
 
   defp deps do
     [
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:plug, "~> 1.3.2 or ~> 1.4", optional: true},
-      {:poison, "~> 3.0", optional: true},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:poison, "~> 3.0", optional: true}
     ]
   end
 end
