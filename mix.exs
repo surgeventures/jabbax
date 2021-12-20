a55ab2150a0def39db584eb698527f91e022b397
Paweł Świątkowski <katafrakt@vivaldi.net>
2021-12-20 16:42:02 +0100

    Add dialyzer checks to CI

--- mix.exs
+++ mix.exs
@@ -12,7 +12,12 @@ defmodule Jabbax.Mixfile do
       description: "JSON API Building Blocks Assembly for Elixir",
       source_url: "https://github.com/surgeventures/jabbax",
       homepage_url: "https://github.com/surgeventures/jabbax",
-      package: package()
+      package: package(),
+      dialyzer: [
+        plt_add_apps: [:ex_unit, :plug],
+        plt_add_deps: :app_tree,
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -35,6 +40,7 @@ defmodule Jabbax.Mixfile do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:plug, "~> 1.12", optional: true},
       {:poison, "~> 3.0", optional: true}
     ]
