269123d9583ba608727325cb1a4a496470021f7d
Kamil Kowalski <kamil@kamilkowalski.pl>
2022-02-14 12:13:38 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jabbax.Mixfile do
   def project do
     [
       app: :jabbax,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
